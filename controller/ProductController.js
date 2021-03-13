4213280d531319c4c930028c0ceb2f7322c4f274
Farhan Rabbaanii <farhanrbnn@gmail.com>
2021-03-13 20:58:56 +0700

    add delete product

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -18,6 +18,51 @@ const input_items = async(req,res) => {
 	}
 }
 
+const get_all_product = async (req, res) => {
+	try {
+		let getData = await Post.find()
+
+		if (getData) {
+			return statusController.success200(res, getData)
+
+		} else {
+			return statusController.badRequest400(res, err)
+
+		}
+	} catch (err) {
+		res.json({
+			'message':err
+		})
+	}
+}
+
+const delete_product = async (req, res) => {
+	let product = req.params.id
+	console.log(product)
+
+	try {
+		let deleteData = await Post.findByIdAndRemove({_id: product})
+
+		if (deleteData) {
+			res.json({
+				'message':'succes delete data'
+			})
+		}
+		else {
+			res.json({
+				'message':'error'
+			})
+		}
+
+	} catch(err) {
+		res.json({
+			'message':err
+		})
+	}
+}
+
 module.exports = {
-	input_items
+	input_items,
+	get_all_product,
+	delete_product
 }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const productController = require('../controller/productController')
 
 router.post('/product/post', productController.input_items)
+router.get('/product/list', productController.get_all_product)
+router.delete('/product/delete/:id', productController.delete_product)
 
 module.exports = router
